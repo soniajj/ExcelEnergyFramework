b56a72bc6b8b7d2f740ee5b3b5e45ac132fa7c64
Jannatul Ferdous <Jannatul Ferdous@192.168.0.236>
2024-03-13 06:36:55 -0500

    Locators

--- ExcelEnergy_HybridFramework/src/test/java/com/objectrepository/Locators.java
+++ ExcelEnergy_HybridFramework/src/test/java/com/objectrepository/Locators.java
@@ -5,12 +5,9 @@ import org.openqa.selenium.By;
 public class Locators {
 
 	public final By signIN_From = By.linkText("Sign In");
-	
-	
-	
-	
-	
-	
+	public final By userName_box = By.xpath("//*[@id=\"xcelenergy-RegistrationLogin\"]/div[1]/form/div[1]/div[4]/input");
+	public final By password_box = By.xpath("//*[@id=\"gigya-password-77105095086979630\"]");
+	public final By sign_In_button = By.xpath("//*[@id=\"gigya-login-form\"]/div[1]/div[9]/input");
 	
 	
 	
